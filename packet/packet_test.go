d70cd5a7baa0567e5fdce394040e883754260839
Athavan Kanapuli <athavankanapuli@gmail.com>
2024-10-13 12:02:51 +0200

    Add more tests for shift bits

--- packet/packet_test.go
+++ packet/packet_test.go
@@ -13,10 +13,10 @@ func TestShiftNBits(t *testing.T) {
 		n              int
 	}{
 		{
-			name:           "Shift 4 bits right",
+			name:           "Shift 1 bits right",
 			packet:         0b11110000,
-			expectedPacket: 0b1111,
-			n:              4,
+			expectedPacket: 0b01111000,
+			n:              1,
 		},
 		{
 			name:           "Shift 2 bits right",
@@ -25,9 +25,39 @@ func TestShiftNBits(t *testing.T) {
 			n:              2,
 		},
 		{
+			name:           "Shift 3 bits right",
+			packet:         0b11110000,
+			expectedPacket: 0b00011110,
+			n:              3,
+		},
+		{
+			name:           "Shift 4 bits right",
+			packet:         0b11110000,
+			expectedPacket: 0b1111,
+			n:              4,
+		},
+		{
+			name:           "Shift 5 bits right",
+			packet:         0b11110000,
+			expectedPacket: 0b0111,
+			n:              5,
+		},
+		{
+			name:           "Shift 6 bits right",
+			packet:         0b11110000,
+			expectedPacket: 0b0011,
+			n:              6,
+		},
+		{
+			name:           "Shift 7 bits right",
+			packet:         0b11110000,
+			expectedPacket: 0b0001,
+			n:              7,
+		},
+		{
 			name:           "Shift 8 bits right",
 			packet:         0b11110000,
-			expectedPacket: 0b00000000,
+			expectedPacket: 0b0000,
 			n:              8,
 		},
 	}
